1f48ddd22cde779ca7836a632b0212df7a324589
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-12 21:15:16 +0000

    Creating all the controller classes

--- src/main/java/com/example/loginsystem/SignUpController.java
+++ src/main/java/com/example/loginsystem/SignUpController.java
@@ -1,4 +1,71 @@
 package com.example.loginsystem;
 
-public class SignUpController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignUpController implements Initializable {
+
+    @FXML
+    private Button button_signup;
+
+    @FXML
+    private Button button_log_in;
+
+    @FXML
+    private RadioButton rb_wittcode;
+
+    @FXML
+    private RadioButton rb_other;
+
+    @FXML
+    private TextField tf_username;
+
+    @FXML
+    private TextField tf_password;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // if the radio buttons were not in the toggle group then they would be able to bothj be selected
+        ToggleGroup toggleGroup = new ToggleGroup();
+        rb_wittcode.setToggleGroup(toggleGroup);
+        rb_other.setToggleGroup(toggleGroup);
+
+        // this one is selected when you open up
+        rb_wittcode.setSelected(true);
+
+
+        button_signup.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // Make sure all the fields are filled in to register
+                String toggleName = ((RadioButton) toggleGroup.getSelectedToggle()).getText();
+                // to remove white spaces in the username
+
+                if (!tf_username.getText().trim().isEmpty() && !tf_password.getText().trim().isEmpty()) {
+                    DBUtils.signUpUser(event, tf_username.getText(), tf_password.getText(), toggleName);
+                } else {
+                    System.out.println("Please fill in all information!");
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("Please fill in all information to sign up!");
+                    alert.show();
+                }
+            }
+        });
+
+        button_log_in.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "hello-view.fxml", "Log in!",  null, null);
+            }
+        });
+
+
+
+    }
 }
