f139e817b42bb9a9585eb6c066d2620e4ab08974
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-12 19:01:30 +0000

    adding widgets to the LoggedInController and set user information method to the class

--- src/main/java/com/example/loginsystem/LoggedInController.java
+++ src/main/java/com/example/loginsystem/LoggedInController.java
@@ -1,4 +1,43 @@
 package com.example.loginsystem;
 
-public class LoggedInController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoggedInController implements Initializable {
+
+
+    @FXML
+    private Button button_logout;
+
+    @FXML
+    private Label label_welcome;
+
+    @FXML
+    private Label label_fav_channel;
+
+
+
+    // allows use to interact with the widgets
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        button_logout.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+            }
+        });
+
+    }
+
+    public void setUserInformation(String username, String favChannel){
+        label_welcome.setText("Welcome " + username + "!");
+       label_fav_channel.setText("Your favorite YouTube channel is " + favChannel + "!");
+    }
 }
